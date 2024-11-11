726754fd5a41613bb8034a4e7e066a30a07cdcee
Riki <rikiandrianakuliah@gmail.com>
2024-11-11 16:35:51 +0700

    done

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const express = require("express");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
+
+//model
+const Product = require("./models/product");
 
 //connect to mongodb
 mongoose
@@ -15,6 +19,8 @@ mongoose
 
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.listen(3000, () => {
   console.log("show app listening  on http://127.0.0.1:3000");
@@ -22,4 +28,49 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index.ejs", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index.ejs", { products, category: "All" });
+  }
+});
+
+app.get("/products/create", (req, res) => {
+  res.render("products/create.ejs");
+});
+
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    ruValidators: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products");
 });
--- models/product.js
+++ models/product.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  brand: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  color: {
+    type: String,
+    require: true,
+  },
+  category: [
+    {
+      type: String,
+      enum: ["Baju", "Celana", "Aksesoris", "Jacket"],
+      require: true,
+    },
+  ],
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
